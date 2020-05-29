66f212e7e350cc37b3062872105d4d3e6dc626be
ced <zuger.cedric@gmail.com>
2020-05-29 07:27:08 +0200

    Basic add mechanism working.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,11 +4,24 @@ const set_tasks_vue = function(){
         el: "#add_tasks_bar",
         data: {
             new_task: null,
-            duration: null
+            duration: null,
+            task_id: 1
         },
         methods: {
             add_task: function (event) {
                 console.log( v.new_task, v.duration );
+
+                var _tm = $( '#task_model' ).clone();
+                var _tl = _tm.find( '#task_libe' );
+
+                _tl.html( v.new_task );
+
+                _tm.attr( 'id', 'task_model_' + v.task_id );
+                _tm.attr( 'id', 'task_libe_' + v.task_id );
+
+                $( '#tasks_list' ).append( _tm );
+
+                v.task_id += 1;
             }
         }
     });
