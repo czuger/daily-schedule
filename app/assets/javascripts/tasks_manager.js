991f779946dbb1cbdd8c2e6350df09368c203dd7
ced <zuger.cedric@gmail.com>
2020-05-29 10:07:09 +0200

    Task order is now separated from task id.

--- app/assets/javascripts/tasks_manager.js
+++ app/assets/javascripts/tasks_manager.js
@@ -4,12 +4,13 @@
 
 class CustomTask {
 
-    constructor( task_desc, task_duration ){
+    constructor( id, task_desc, task_duration ){
         this.task_desc = task_desc;
         this.task_duration = task_duration;
+        this.task_key = 'task_' + id;
     }
 
-    show_task( index, previous_time ){
+    show_task( previous_time ){
         var _tm = $( '#task_model' ).clone();
         var _tl = _tm.find( '#task_desc' );
         var _td = _tm.find( '#task_duration' );
@@ -24,28 +25,29 @@ class CustomTask {
 
         _tl.html( this.task_desc );
         _td.val( this.task_duration );
-        _td.attr( 'task_id', index );
+        _td.attr( 'task_id', this.task_key );
 
         _ts.html( start_time.toLocaleString({ hour: 'numeric', minute: 'numeric' }));
         _te.html( end_time.toLocaleString({ hour: 'numeric', minute: 'numeric' }));
 
-        _tm.attr( 'id', 'task_model_' + index );
-        _tl.attr( 'id', 'task_desc_' + index );
-        _td.attr( 'id', 'task_duration_' + index );
-        _ts.attr( 'id', 'task_start_' + index );
-        _te.attr( 'id', 'task_end_' + index );
+        _tm.attr( 'id', 'task_model_' + this.task_key );
+        _tl.attr( 'id', 'task_desc_' + this.task_key );
+        _td.attr( 'id', 'task_duration_' + this.task_key );
+        _ts.attr( 'id', 'task_start_' + this.task_key );
+        _te.attr( 'id', 'task_end_' + this.task_key );
 
         $( '#tasks_list' ).append( _tm );
 
         return end_time;
     }
-
 }
 
 class TasksManager {
 
     constructor(){
-        this.tasks = [];
+        this.tasks_unique_id = 0;
+        this.tasks = {};
+        this.tasks_order = [];
         this.day_start_time = luxon.DateTime.local();
 
         // console.log( this.day_start_time );
@@ -57,7 +59,10 @@ class TasksManager {
     }
 
     addTask( task_desc, task_duration ){
-        this.tasks.push( new CustomTask( task_desc, task_duration ) );
+        var _t = new CustomTask( this.tasks_unique_id, task_desc, task_duration );
+        this.tasks[ _t.task_key ] = _t;
+        this.tasks_order.push( _t.task_key );
+        this.tasks_unique_id += 1;
     }
 
     changeDuration( task_id, task_duration ){
@@ -70,8 +75,8 @@ class TasksManager {
 
         var previous_time = this.day_start_time;
 
-        this.tasks.forEach(function (item, index) {
-            previous_time = item.show_task( index, previous_time );
-        });
+        for (const task_key of this.tasks_order) {
+            previous_time = this.tasks[ task_key ].show_task( previous_time );
+        }
     }
 }
