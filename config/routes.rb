8fd84dff834d8964c65b32ce25a777e9bf6296df
ced <zuger.cedric@gmail.com>
2020-06-18 12:15:57 +0200

    Rails removed.

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'tasks/show'
-  post 'tasks/create'
-  get 'tasks/load'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root 'tasks#show'
-end
